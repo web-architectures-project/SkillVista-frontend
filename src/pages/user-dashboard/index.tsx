47d075760b7bfce4fe9bea17445094f9e9a76c37
Saul Burgess <Burgesssaul@gmail.com>
2023-10-05 13:35:37 +0100

    Renames "index" to "Index" to conform to eslint

--- src/pages/user-dashboard/index.tsx
+++ src/pages/user-dashboard/index.tsx
@@ -2,16 +2,16 @@ import { AuthContext } from "@/context/auth-context";
 import { useRouter } from "next/router";
 import { FC, useContext, useEffect } from "react";
 
-interface indexProps {}
+interface IndexProps {}
 
-const index: FC<indexProps> = ({}) => {
+const Index: FC<IndexProps> = ({}) => {
   const router = useRouter();
   const authContext = useContext(AuthContext);
   useEffect(() => {
     console.log("working");
     !authContext?.isUserAuthenticated && router.push("/login");
-  }, []);
+  }, [authContext?.isUserAuthenticated, router]);
   return <div>User Dashboard</div>;
 };
 
-export default index;
+export default Index;
