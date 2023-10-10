d9d29728b595f6a8ccfb00d24d009d7894a1e9f3
call203 <proground.developer@gmail.com>
2023-10-10 23:59:33 +0100

    Add/Header&Footer

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+const Header = () => {
+  return (
+    <header className="flex flex-col flex-wrap bg-white px-8 py-6 sticky top-0">
+      <div className="flex items-center flex-shrink-0 mr-10">
+        <span className="font-semibold text-xl">SkillVista</span>
+      </div>
+    </header>
+  )
+}
+
+export default Header
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,17 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import Footer from '@/components/Footer'
+import Header from '@/components/Header'
+import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <Header />
       <body className="debug-screens">
         <Main />
         <NextScript />
       </body>
+      <Footer />
     </Html>
-  );
+  )
 }
