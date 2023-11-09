3de79516c65b674c2c6cefb157994102ee16cb7a
Soyeon Lee <57412714+call203@users.noreply.github.com>
2023-11-09 11:29:17 +0000

    Fix/login (#27)
    * login error
    
    * fix/login
    
    ---------
    
    Co-authored-by: call203 <proground.developer@gmail.com>

--- src/pages/user-dashboard/index.tsx
+++ src/pages/user-dashboard/index.tsx
@@ -1,78 +1,77 @@
-import { DataTable } from "@/components/user-dashboard/DataTable";
-import FullScreenSearchBar from "@/components/user-dashboard/FullScreenSearchBar";
-import { columns } from "@/components/user-dashboard/columns";
-import { DummyData } from "@/lib/utils/UserDashboardData";
-import { NextApiRequest, NextApiResponse } from "next";
-import { FC, useEffect, useState } from "react";
-import { Modal } from "@/components/ui/modal";
-import { useRouter } from "next/router";
-import { getCookies, setCookie } from "cookies-next";
-import { useQuery } from "@tanstack/react-query";
-import { useSelector } from "react-redux";
-import { selectAuthState } from "@/store/authSlice";
+import { DataTable } from '@/components/user-dashboard/DataTable'
+import FullScreenSearchBar from '@/components/user-dashboard/FullScreenSearchBar'
+import { columns } from '@/components/user-dashboard/columns'
+import { DummyData } from '@/lib/utils/UserDashboardData'
+import { NextApiRequest, NextApiResponse } from 'next'
+import { FC, useEffect, useState } from 'react'
+import { Modal } from '@/components/ui/modal'
+import { useRouter } from 'next/router'
+import { getCookies, setCookie } from 'cookies-next'
+import { useQuery } from '@tanstack/react-query'
+import { useSelector } from 'react-redux'
+import { selectAuthState } from '@/store/authSlice'
 
 interface IndexProps {
-  consent: boolean;
+  consent: boolean
 }
 
 interface ServerSideProps {
-  res: NextApiResponse;
-  req: NextApiRequest;
+  res: NextApiResponse
+  req: NextApiRequest
 }
 
 const Index: FC<IndexProps> = ({ consent }: IndexProps) => {
-  const [cookieModal, setCookieModal] = useState(false);
-  const [query, setQuery] = useState("");
-  const [fetchedData, setFetchedData] = useState({});
-  const router = useRouter();
-  const authState = useSelector(selectAuthState);
+  const [cookieModal, setCookieModal] = useState(false)
+  const [query, setQuery] = useState('')
+  const [fetchedData, setFetchedData] = useState({})
+  const router = useRouter()
+  const authState = useSelector(selectAuthState)
 
   /* Cookie-consent check & Modal */
   useEffect(() => {
-    if (JSON.stringify(consent) === "{}") {
-      setCookieModal(true);
+    if (JSON.stringify(consent) === '{}') {
+      setCookieModal(true)
     }
-  }, [consent]);
+  }, [consent])
 
   const rightFunc = () => {
-    setCookieModal(false);
-  };
+    setCookieModal(false)
+  }
 
   const leftFunc = async () => {
-    setCookie("consent", true, {
+    setCookie('consent', true, {
       //1 year
       expires: new Date(Number(new Date()) + 315360000000),
-    });
-    setCookieModal(false);
-  };
+    })
+    setCookieModal(false)
+  }
 
   const fetchDataOnEnter = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    setFetchedData(data?.data);
-  };
+    event.preventDefault()
+    setFetchedData(data?.data)
+  }
 
   useEffect(() => {
-    if (query === "") setFetchedData("");
-  }, [fetchedData, query]);
+    if (query === '') setFetchedData('')
+  }, [fetchedData, query])
 
   // useEffect to check user authentication and redirect if not authenticated
   useEffect(() => {
-    if (!authState) router.push("user-login");
-
+    // if (!authState) router.push("user-login");
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [authState]);
+  }, [authState])
 
   return (
     // Main container for the component
     <main className="container">
       {cookieModal && (
         <Modal
-          title={"Cookie Consent"}
+          title={'Cookie Consent'}
           content={
-            "This website use cookies to help you have a superior and more admissible browsing experience on the website."
+            'This website use cookies to help you have a superior and more admissible browsing experience on the website.'
           }
-          rightButton={"Decline"}
-          leftButton={"Accept"}
+          rightButton={'Decline'}
+          leftButton={'Accept'}
           rightFunc={rightFunc}
           leftFunc={leftFunc}
         />
@@ -87,24 +86,24 @@ const Index: FC<IndexProps> = ({ consent }: IndexProps) => {
             fetchDataOnEnter={fetchDataOnEnter}
           />
 
-          <DataTable
+          {/* <DataTable
             columns={columns}
             data={DummyData}
             query={query}
             queryData={fetchedData}
-          />
+          /> */}
         </div>
       </div>
     </main>
-  );
-};
+  )
+}
 
 // Export the Index component as the default export
-export default Index;
+export default Index
 
 export async function getServerSideProps({ req, res }: ServerSideProps) {
-  const data = getCookies({ req, res });
-  const consent = data["consent"] || {};
+  const data = getCookies({ req, res })
+  const consent = data['consent'] || {}
 
-  return { props: { consent } };
+  return { props: { consent } }
 }
