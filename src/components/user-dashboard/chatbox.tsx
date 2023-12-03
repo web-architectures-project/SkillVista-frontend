b22529f2612d2f77a1794e607b768aab28634d08
call203 <proground.developer@gmail.com>
2023-12-03 12:26:30 +0000

    add/chat

--- src/components/user-dashboard/chatbox.tsx
+++ src/components/user-dashboard/chatbox.tsx
@@ -5,11 +5,21 @@ import { apiRequest } from '../apis/default'
 import { useDispatch, useSelector } from 'react-redux'
 import { Chat, selectChatListState, setChatListState } from '@/store/chatSlilce'
 import { selectUserId } from '@/store/userSlice'
+import { useRouter } from 'next/router'
 
-export interface ChatBoxProps {}
+export interface ChatBoxProps {
+  handleChatBox: () => void
+  providerId: number
+  providername: string
+}
 
-const ChatBox = () => {
+const ChatBox = ({ handleChatBox, providerId, providername }: ChatBoxProps) => {
   const chatlist = useSelector(selectChatListState)
+
+  const testProvidername = 'call203'
+  const testProviderId = 2
+  const router = useRouter()
+  // const userId = 8
 
   const userId = useSelector(selectUserId)
   const dispatch = useDispatch()
@@ -21,7 +31,13 @@ const ChatBox = () => {
       path: '/contacts',
     }).then(res => {
       if (res?.status === 200) {
-        dispatch(setChatListState(res.message))
+        const messages = res.message.filter(
+          (item: Chat) => item.provider_id === testProviderId && item.user_id === userId,
+        )
+        console.log(userId)
+        console.log(testProviderId)
+        console.log(messages)
+        dispatch(setChatListState(messages))
       }
     })
   }, [dispatch])
@@ -37,7 +53,7 @@ const ChatBox = () => {
   const handlePressSend = () => {
     const chatform = {
       user_id: userId,
-      provider_id: chatlist[0].provider_id,
+      provider_id: testProviderId,
       who: userId === chatlist[0].provider_id ? 'service_provider' : 'user',
       message_content: message,
       date_sent: new Date().toISOString(),
@@ -55,23 +71,29 @@ const ChatBox = () => {
     })
   }
 
+  useEffect(() => {
+    setInterval(() => {
+      getMessages()
+    }, 10000)
+  }, [getMessages])
+
   return (
     <div className="flex flex-col lg:w-2/5 lg:h-3/5 h-2/4  py-7 px-7 mx-3 fixed inset-x-0 bottom-0  z-50 bg-white rounded-xl shadow-2xl">
       <div className="flex justify-between items-stretch pb-3">
-        <div className=" text-lg font-bold ">Message Kamran</div>
+        <div className=" text-lg font-bold ">Message {providername}</div>
         <div className="self-center">
-          <Image src={'/close.png'} width={18} height={18} alt="close" />
+          <Image src={'/close.png'} width={18} height={18} alt="close" onClick={handleChatBox} />
         </div>
       </div>
 
       <hr className="bg-black" />
       <div className="overflow-y-auto pb-20 flex-grow ">
-        <div className="flex flex-col">
+        <div className="flex flex-col px-3">
           {chatlist?.length > 0 &&
             chatlist.map((item: Chat, index: number) => {
-              const conditionPotion = item.user_id === userId ? 'justify-end' : 'justify-start'
+              const conditionPotion = item.who === 'user' ? 'justify-end' : 'justify-start'
               const conditionBackground =
-                item.user_id === 8 ? 'bg-mainblue text-white' : 'bg-slate-300'
+                item.who === 'user' ? 'bg-mainblue text-white' : 'bg-slate-300'
               return (
                 <div key={index} className={`py-3 flex ${conditionPotion}`}>
                   <span className={`rounded-3xl  px-4 py-2 ${conditionBackground}`}>
